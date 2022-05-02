b14c0bdfee5c9aba88d05e65b6d96d2889387016
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-05-02 12:04:52 +0000

    [Finish]2-9

---
+++ new.rb
@@ -0,0 +1,34 @@
+# インスタンスメソッド
+class Car
+  
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+  
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+car = Car.new
+car.move("右", 5)
+
+
+# クラスメソッド
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+  
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+Car.run(10)
+Car.turn("右")
